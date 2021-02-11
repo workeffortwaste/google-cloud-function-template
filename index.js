8ad37cac15f8b7119c9bfbf311ca02f8c3c1bb7b
Chris Johnson <49479599+workeffortwaste@users.noreply.github.com>
2021-02-11 09:48:40 +0000

    Add basic CORS support

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ if (!process.env.GCP_PROJECT) {
 }
 
 exports.helloWorld = (req, res) => {
-  res.send('Hello, World')
+  // Handle CORS
+  res.set('Access-Control-Allow-Origin', '*')
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'POST')
+    res.set('Access-Control-Allow-Headers', 'Content-Type')
+    res.set('Access-Control-Max-Age', '3600')
+    res.status(204).send('')
+  } else {
+    res.send('Hello, World')
+  }
 }
